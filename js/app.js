c630a44b9799d5a37512e540924d76a190e64372
Dave Strus <dave@getfretless.com>
2016-06-01 13:43:07 -0400

    Display values in a definition list.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation()
+
+var myForm = document.querySelector('form');
+myForm.onsubmit = function(ev) {
+  ev.preventDefault();
+  var details = document.querySelector('div.details');
+
+  var firstName = this.firstName.value;
+  var hairColor = this.hairColor.value;
+  var age = this.age.value;
+  var birthplace = this.birthplace.value;
+
+  details.innerHTML += ' \
+    <dl> \
+      <dt>First Name</dt> \
+      <dd>' + firstName + '</dd> \
+        \
+      <dt>Hair Color</dt> \
+      <dd>' + hairColor + '</dd> \
+        \
+      <dt>Age</dt> \
+      <dd>' + age + '</dd> \
+        \
+      <dt>Birthplace</dt> \
+      <dd>' + birthplace + '</dd> \
+        \
+    </dl>';
+};
