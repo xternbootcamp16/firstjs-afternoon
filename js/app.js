ff19ca645adcbc337886034198baf5bcca983586
Dave Strus <dave@getfretless.com>
2016-06-01 14:37:34 -0400

    Extract buildList function.

--- js/app.js
+++ js/app.js
@@ -8,32 +8,37 @@ var buildColorDiv = function(color) {
   return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
 };
 
+var buildList = function(listValues) {
+  return ' \
+    <dl> \
+      <dt>First Name</dt> \
+      <dd>' + listValues.firstName + '</dd> \
+        \
+      <dt>Hair Color</dt> \
+      <dd>' + listValues.hairColor + '</dd> \
+        \
+      <dt>Age</dt> \
+      <dd>' + listValues.age + '</dd> \
+        \
+      <dt>Birthplace</dt> \
+      <dd>' + listValues.birthplace + '</dd> \
+        \
+    </dl>';
+};
+
 var addValuesToDetails = function(ev) {
   ev.preventDefault();
   var details = document.querySelector('div.details');
-
-  var firstName = this.firstName.value;
   var hairColor = this.hairColor.value;
-  var age = this.age.value;
-  var birthplace = this.birthplace.value;
 
-  var colorDiv = buildColorDiv(hairColor);
+  var listValues = {
+    firstName: this.firstName.value,
+    age: this.age.value,
+    birthplace: this.birthplace.value,
+    hairColor: buildColorDiv(hairColor)
+  };
 
-  details.innerHTML += ' \
-    <dl> \
-      <dt>First Name</dt> \
-      <dd>' + firstName + '</dd> \
-        \
-      <dt>Hair Color</dt> \
-      <dd>' + colorDiv + '</dd> \
-        \
-      <dt>Age</dt> \
-      <dd>' + age + '</dd> \
-        \
-      <dt>Birthplace</dt> \
-      <dd>' + birthplace + '</dd> \
-        \
-    </dl>';
+  details.innerHTML += buildList(listValues);
 };
 
 var myForm = document.querySelector('form');
