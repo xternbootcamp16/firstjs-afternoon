095d46790e7f052c16467251c6988e89bfa465a0
Dave Strus <dave@getfretless.com>
2016-06-01 15:28:30 -0400

    Wrap in an IIFE to avoid global variables.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
 $(document).foundation();
 
-var buildColorDiv = function(color) {
-  return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
-};
-
-var buildList = function(listValues) {
-  var dl = document.createElement('dl');
-  dl.style.border = '1px solid red';
-  dl.innerHTML = buildListItem('First Name', listValues.firstName) +
-    buildListItem('HairColor', listValues.hairColor) +
-    buildListItem('Age', listValues.age) +
-    buildListItem('Birthplace', listValues.birthplace);
-
-  return dl;
-};
-
-var buildListItem = function(term, definition) {
-  var li = ' \
-    <li> \
-      <dt>' + term + '</dt> \
-      <dd>' + definition + '</dd> \
-    </li>';
-  return li;
-};
-
-var addValuesToDetails = function(ev) {
-  ev.preventDefault();
-  var details = document.querySelector('div.details');
-  var hairColor = this.hairColor.value;
-
-  var listValues = {
-    firstName: this.firstName.value,
-    age: this.age.value,
-    birthplace: this.birthplace.value,
-    hairColor: buildColorDiv(hairColor)
+(function() {
+  var buildColorDiv = function(color) {
+    return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
   };
 
-  details.appendChild(buildList(listValues));
-};
+  var buildList = function(listValues) {
+    var dl = document.createElement('dl');
+    dl.style.border = '1px solid red';
+    dl.innerHTML = buildListItem('First Name', listValues.firstName) +
+      buildListItem('HairColor', listValues.hairColor) +
+      buildListItem('Age', listValues.age) +
+      buildListItem('Birthplace', listValues.birthplace);
 
-var myForm = document.querySelector('form');
-myForm.onsubmit = addValuesToDetails;
+    return dl;
+  };
+
+  var buildListItem = function(term, definition) {
+    var li = ' \
+      <li> \
+        <dt>' + term + '</dt> \
+        <dd>' + definition + '</dd> \
+      </li>';
+    return li;
+  };
+
+  var addValuesToDetails = function(ev) {
+    ev.preventDefault();
+    var details = document.querySelector('div.details');
+    var hairColor = this.hairColor.value;
+
+    var listValues = {
+      firstName: this.firstName.value,
+      age: this.age.value,
+      birthplace: this.birthplace.value,
+      hairColor: buildColorDiv(hairColor)
+    };
+
+    details.appendChild(buildList(listValues));
+  };
+
+  var myForm = document.querySelector('form');
+  myForm.onsubmit = addValuesToDetails;
+})();
